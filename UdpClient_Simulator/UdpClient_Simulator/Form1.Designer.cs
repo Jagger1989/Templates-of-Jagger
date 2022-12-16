a9873f5cc9b28bac93b60bf5550bc102a5afe65b
xiao.liang0701 <86827453+Jagger1989@users.noreply.github.com>
2022-12-16 14:11:24 +0800

    Added udp client & server

---
+++ UdpClient_Simulator/UdpClient_Simulator/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace UdpClient_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SendMsg = new System.Windows.Forms.TextBox();
+            this.sendedMsg = new System.Windows.Forms.TextBox();
+            this.sendFromTexbox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Location = new System.Drawing.Point(40, 112);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(75, 23);
+            this.SendBtn.TabIndex = 0;
+            this.SendBtn.Text = "Send";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            this.SendBtn.Click += new System.EventHandler(this.sendFromFile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "load data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.loadData);
+            // 
+            // SendMsg
+            // 
+            this.SendMsg.Location = new System.Drawing.Point(40, 200);
+            this.SendMsg.Multiline = true;
+            this.SendMsg.Name = "SendMsg";
+            this.SendMsg.Size = new System.Drawing.Size(500, 450);
+            this.SendMsg.TabIndex = 2;
+            // 
+            // sendedMsg
+            // 
+            this.sendedMsg.Location = new System.Drawing.Point(640, 200);
+            this.sendedMsg.Multiline = true;
+            this.sendedMsg.Name = "sendedMsg";
+            this.sendedMsg.Size = new System.Drawing.Size(500, 450);
+            this.sendedMsg.TabIndex = 3;
+            // 
+            // sendFromTexbox
+            // 
+            this.sendFromTexbox.Location = new System.Drawing.Point(40, 153);
+            this.sendFromTexbox.Name = "sendFromTexbox";
+            this.sendFromTexbox.Size = new System.Drawing.Size(75, 23);
+            this.sendFromTexbox.TabIndex = 4;
+            this.sendFromTexbox.Text = "Send Box";
+            this.sendFromTexbox.UseVisualStyleBackColor = true;
+            this.sendFromTexbox.Click += new System.EventHandler(this.sendFromTextbox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.sendFromTexbox);
+            this.Controls.Add(this.sendedMsg);
+            this.Controls.Add(this.SendMsg);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SendBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox SendMsg;
+        private System.Windows.Forms.TextBox sendedMsg;
+        private System.Windows.Forms.Button sendFromTexbox;
+    }
+}
+
---
+++ UdpClient_Simulator/UdpClient_Simulator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdpClient_Simulator
+{
+    public partial class Form1 : Form
+    {
+        private Socket m_UdpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private EndPoint m_ServerPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55555);
+        private string m_msg = "Empty message !";
+        private string m_SileContent = string.Empty;
+        private string m_FilePath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sendFromFile(object sender, EventArgs e)
+        {
+            if (m_FilePath == "")
+            {
+                MessageBox.Show("Please load data file first !");
+                return;
+            }
+            FileStream fs = new FileStream(m_FilePath, FileMode.Open, FileAccess.Read);
+            byte[] data = new byte[fs.Length];
+            fs.Read(data, 0, data.Length);
+            m_UdpClient.SendTo(data, m_ServerPoint);
+            m_msg = Encoding.UTF8.GetString(data);
+            SendMsg.Text = m_msg;
+            sendedMsg.AppendText("\r\n");
+            sendedMsg.AppendText(m_msg);
+            fs.Close();
+        }
+
+        ~Form1()
+        {
+            m_UdpClient.Close();
+        }
+
+        private void loadData(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    m_FilePath = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void sendFromTextbox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
